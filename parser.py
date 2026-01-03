89929b03dc390896c10b619f4e6f0dd79f493112
MaryKateEvan <marykate_evan@outlook.com.gr>
2026-01-03 03:18:51 +0100

    okay now the equation= without anything after the equal throws an error

--- parser.py
+++ parser.py
@@ -1,3 +1,18 @@
+def parse_each_side(side: str) -> dict[int, float]:
+	# 1) we trim all the spaces:
+	side = side.replace(" ", "")
+	# 2) we convert the format "a - b + c - d" to "a + -b + c + -d"
+	side = side.replace("-", "+-")
+	# 2b) fix the edge case of the beginning with "-"
+	if side.startswith("+-"):
+		side = side[1:]
+	# 3) split the side parts by the +:
+	side_parts = side.split("+")
+
+	poly_dict: dict[int, float] = {}
+	return poly_dict
+
+
 def parse_equation(equation: str):
 	# print("Received equation:", equation)
 
@@ -6,6 +21,14 @@ def parse_equation(equation: str):
 	sides = equation.split('=')
 	if len(sides) != 2:
 		raise ValueError("Equation must contain exactly one '='.")
+	
+	# my main polyonym dictionary to fill:
+	poly_dict: dict[int, float] = {} # equivalent to the `std::map<int, double> poly;` i knew
 
 	for side in sides:
-		print(side)
+		side = side.strip()
+		print("side:", side, "$")
+		if side is None or side == "":
+			raise ValueError("One side is missing from the equation")
+	
+	# converts the "a - b + c - d" to "a + -b + c + -d" to safely split then by "+"
