4f09d0d18757d96a9b8e9990186ef2ddf175220e
MaryKateEvan <marykate_evan@outlook.com.gr>
2026-01-03 02:17:22 +0100

    try and except ad the throw and catch in cpp. I like this approach. Looks clean instead of printing the error and exiting from the parser

--- parser.py
+++ parser.py
@@ -1,2 +1,11 @@
 def parse_equation(equation: str):
-	print("Received equation:", equation)
+	# print("Received equation:", equation)
+
+	equation = equation.strip() #trims unnecessary whitespaces
+
+	sides = equation.split('=')
+	if len(sides) != 2:
+		raise ValueError("Equation must contain exactly one '='.")
+
+	for side in sides:
+		print(side)
